17f23a04e94df16ae4f16a4b83659ce1c02830fe
panbin <1764183867@qq.com>
2021-11-15 12:44:38 +0800

    Add MarioMaker

--- example/HelloBot/Program.cs
+++ example/HelloBot/Program.cs
@@ -14,12 +14,24 @@ namespace HelloBot
             bot.ChatlMsg += ChatMsg;
             bot.Run();
         }
-
+        /// <summary>
+        /// 私聊消息
+        /// </summary>
+        /// <param name="chat"></param>
+        /// <param name="targetID"></param>
         private static void ChatMsg(string chat, string targetID)
         {
-           //登录
+            Console.WriteLine("Bot:" + chat);
+            if (chat == "你好")
+            {
+                bot.SendMessage.Chat(targetID, "我是机器人。你好");
+            }
         }
-
+        /// <summary>
+        /// 频道消息
+        /// </summary>
+        /// <param name="chat"></param>
+        /// <param name="targetID"></param>
         private static void ChannelReceive(string chat, string targetID)
         {
             Console.WriteLine("Bot:" + chat);
---
+++ example/MarioMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarioMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
