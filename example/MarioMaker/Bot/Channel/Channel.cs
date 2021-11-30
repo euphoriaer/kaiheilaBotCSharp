2af7443c01503547264924b82af3cfd4e590fa1a
panbin <1764183867@qq.com>
2021-11-30 13:56:14 +0800

    Add Space Support

--- example/MarioMaker/Bot/Channel/Channel.cs
+++ example/MarioMaker/Bot/Channel/Channel.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
 using System.Net.Http;
 
 namespace MarioMaker
@@ -10,14 +9,13 @@ namespace MarioMaker
         [AttrMario(".add.GROUP")]
         public static void AddGuanqia(JToken jObject)
         {
+            string wholeMsg = jObject["content"].ToString();
+            int spaceIndex = wholeMsg.IndexOf(" ");//定位第一个空格
+            var msgs = wholeMsg.Substring(spaceIndex).Split(@"\");//空格后面的是参数
 
-            var msgs = jObject["content"].ToString().Split(" ");
             string kaiheilaId = jObject["author_id"].ToString();
             string targetID = jObject["target_id"].ToString();
 
-            
-
-            
             JObject msgJobj = new JObject();
             msgJobj.Add("levelId", msgs[1]);
             msgJobj.Add("levelName", msgs[2]);
@@ -32,23 +30,23 @@ namespace MarioMaker
                 httpRequestMessage.Content.Headers.Remove("Content-type");
                 httpRequestMessage.Content.Headers.Add("Content-type", "application/json");
                 var result = client.SendAsync(httpRequestMessage); //返回结果
-                var res = result.Result.Content.ReadAsStringAsync();          
+                var res = result.Result.Content.ReadAsStringAsync();
                 res.Wait();
 
                 JToken rem = JsonConvert.DeserializeObject<JToken>(res.Result);//解析返回的json
                 if (rem["code"].ToString() == "0")
                 {
                     JToken js1 = JsonConvert.DeserializeObject<JToken>(RegSuccess);
-                    string m = "**关卡id：" + rem["data"]["levelId"] + "**\n**关卡名字：" + rem["data"]["levelName"] + "**\n**关卡类型：" + rem["data"]["levelTypeStr"]+ "**\n***                                   成  功  上  传  ！！！！！***";
+                    string m = "**关卡id：" + rem["data"]["levelId"] + "**\n**关卡名字：" + rem["data"]["levelName"] + "**\n**关卡类型：" + rem["data"]["levelTypeStr"] + "**\n***                                   成  功  上  传  ！！！！！***";
                     js1[0]["modules"][0]["text"]["content"] = m;
                     string json1 = JsonConvert.SerializeObject(js1);//初始化注册成功的卡片消息
 
                     JObject dic1 = new JObject();
                     dic1.Add("type", "10");
                     dic1.Add("content", json1);
-                    dic1.Add("target_id", targetID );
+                    dic1.Add("target_id", targetID);
                     string Ress1 = JsonConvert.SerializeObject(dic1);
-                    _bot.SendMessage.Post(_baseUrl+ "/api/v3/message/create", Ress1);
+                    _bot.SendMessage.Post(_baseUrl + "/api/v3/message/create", Ress1);
                 }
                 else if (rem["code"].ToString() == "500")
                 {
@@ -56,14 +54,11 @@ namespace MarioMaker
                     dd.DefaultR(jObject, _bot, rem, targetID);
                 }
 
-
                 if (string.IsNullOrEmpty(res.Result))
                 {
                     _bot.SendMessage.Channel(targetID, "回调错误，post返回为空");
                     return;
                 }
-
-                
             }
         }
     }
